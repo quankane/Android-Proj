25aee4c69dd5f067ed1980d17895e4d0201233b6
quankane <quanducbui2017@gmail.com>
2025-11-06 23:45:35 +0700

    Dashboard UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,13 +41,15 @@ android {
 }
 
 dependencies {
-
+    implementation("com.tbuonomo:dotsindicator:5.1.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.database)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.viewpager2)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
