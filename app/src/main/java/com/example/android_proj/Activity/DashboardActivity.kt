254592a688f2890758a2c233ab587183f1cbeb72
quankane <quanducbui2017@gmail.com>
2025-11-06 22:47:06 +0700

    Config view binding and refactor main to dashboard

--- app/src/main/java/com/example/android_proj/Activity/DashboardActivity.kt
+++ app/src/main/java/com/example/android_proj/Activity/DashboardActivity.kt
@@ -1,12 +1,13 @@
-package com.example.android_proj
+package com.example.android_proj.Activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.android_proj.R
 
-class MainActivity : AppCompatActivity() {
+class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
---
+++ app/src/main/java/com/example/android_proj/Activity/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.example.android_proj.Activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.android_proj.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+    }
+}
