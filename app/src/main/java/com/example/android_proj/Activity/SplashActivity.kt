28f7395c9c203a806893aae853c30948f42d0a4f
quankane <quanducbui2017@gmail.com>
2025-11-06 22:55:17 +0700

    Complete splash activity

--- app/src/main/java/com/example/android_proj/Activity/SplashActivity.kt
+++ app/src/main/java/com/example/android_proj/Activity/SplashActivity.kt
@@ -1,17 +1,25 @@
 package com.example.android_proj.Activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.android_proj.R
+import com.example.android_proj.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener {
+            startActivity(Intent(this@SplashActivity, DashboardActivity::class.java))
+        }
 
     }
 }
