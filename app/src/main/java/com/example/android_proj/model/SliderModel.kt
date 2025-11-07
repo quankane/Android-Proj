e91ecd96fd37277ad67c4a3c0dfad71c0304cfb3
quankane <quanducbui2017@gmail.com>
2025-11-07 16:12:09 +0700

    Add new field repo and viewmodel for banners

---
+++ app/src/main/java/com/example/android_proj/model/SliderModel.kt
@@ -0,0 +1,4 @@
+package com.example.android_proj.model
+
+class SliderModel(val url: String) {
+}
--- app/src/main/java/com/example/android_proj/repository/MainRepository.kt
+++ app/src/main/java/com/example/android_proj/repository/MainRepository.kt
@@ -3,6 +3,7 @@ package com.example.android_proj.repository
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.android_proj.model.BrandModel
+import com.example.android_proj.model.SliderModel
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -12,7 +13,9 @@ class MainRepository {
     private val firebaseDatabase = FirebaseDatabase.getInstance();
 
     private val _brands = MutableLiveData<MutableList<BrandModel>>()
+    private val _banners = MutableLiveData<List<SliderModel>>()
 
+    val banners: LiveData<List<SliderModel>> get() = _banners
     val brands: LiveData<MutableList<BrandModel>> get() = _brands
 
     fun loadBrands() {
@@ -26,6 +29,26 @@ class MainRepository {
                     }
                 }
                 _brands.value = list
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+
+    fun loadBanners() {
+        val ref = firebaseDatabase.getReference("Banner")
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val list = mutableListOf<SliderModel>()
+                for (childSnapshot in snapshot.children) {
+                    childSnapshot.getValue(SliderModel::class.java)?.let {
+                        list.add(it)
+                    }
+                }
+                _banners.value = list
             }
 
             override fun onCancelled(error: DatabaseError) {
