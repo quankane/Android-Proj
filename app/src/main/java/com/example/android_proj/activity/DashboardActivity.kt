87f770a4fe5e3957576a01453adcf1a9df002e5c
quankane <quanducbui2017@gmail.com>
2025-11-07 15:50:07 +0700

    Complete brand list and fix pos center for nav

--- app/src/main/java/com/example/android_proj/activity/DashboardActivity.kt
+++ app/src/main/java/com/example/android_proj/activity/DashboardActivity.kt
@@ -1,21 +1,49 @@
 package com.example.android_proj.activity
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.android_proj.R
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.android_proj.adapter.BrandsAdapter
+import com.example.android_proj.databinding.ActivityMainBinding
+import com.example.android_proj.viewModel.MainViewModel
 
 class DashboardActivity : AppCompatActivity() {
+
+    private val viewModel : MainViewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
+    private lateinit var binding : ActivityMainBinding
+
+    private val brandsAdapter = BrandsAdapter(mutableListOf())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initUI()
+    }
+
+    private fun initUI() {
+        initBrands()
+    }
+
+    private fun initBrands() {
+        binding.recyclerViewBrands.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        binding.recyclerViewBrands.adapter = brandsAdapter
+        binding.progressBarBrands.visibility = View.VISIBLE
+
+        viewModel.brands.observe(this) {
+            data ->
+            brandsAdapter.updateData(data)
+            binding.progressBarBrands.visibility = View.GONE
         }
+        viewModel.loadBrands()
     }
 }
