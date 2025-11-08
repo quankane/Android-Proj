6ea0318a01948e458cd844ba7c93f01a83446f35
quankane <quanducbui2017@gmail.com>
2025-11-08 21:48:56 +0700

    Update activity cart layout xml

--- app/src/main/java/com/example/android_proj/activity/CartActivity.kt
+++ app/src/main/java/com/example/android_proj/activity/CartActivity.kt
@@ -6,16 +6,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.android_proj.R
+import com.example.android_proj.databinding.ActivityCartBinding
+import com.example.android_proj.databinding.ViewholderCartBinding
+import com.example.android_proj.helper.ManagementCart
 
 class CartActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCartBinding
+    private lateinit var managementCart: ManagementCart
+    private var tax: Double = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_cart)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityCartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
