baac677c0d5665576844b554ff844c4d32d7b76a
quankane <quanducbui2017@gmail.com>
2025-11-12 21:56:58 +0700

    Initial add edit user

---
+++ app/src/main/java/com/example/android_proj/activity/admin/AddEditUserActivity.kt
@@ -0,0 +1,21 @@
+package com.example.android_proj.activity.admin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.android_proj.R
+
+class AddEditUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_add_edit_user)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
